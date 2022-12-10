d8884ad1d3a066dd593337e8a6ad7ce409ea34cc
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2022-12-10 23:40:37 +0100

    add city model

--- carpooling/ideas/models.py
+++ carpooling/ideas/models.py
@@ -9,3 +9,9 @@ class User(models.Model):
 
     def __str__(self):
         return f'{self.lname} {self.fname}'
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
