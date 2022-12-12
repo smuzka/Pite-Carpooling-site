d2ce8ed29a98dd1c9a67ff52b8df62a4bd5d891f
Kamil <gatlikkamil@gmail.com>
2022-12-12 19:13:25 +0100

    Added views for login register and logout

--- carpooling/ideas/views.py
+++ carpooling/ideas/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import UserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def home(request):
     return render(request, "homePage.html")
 
-def login(request):
-    return render(request, "loginPage.html")
 
-def register(request):
-    return render(request, "registerPage.html")
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/logged")
+
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request=request, template_name="loginPage.html", context={"login_form": form})
+
+
+# TODO: match form fields in registerPage with UserForm
+# TODO: override save method to save record to db
+def register_request(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/logged")
+    form = UserForm()
+    return render(request=request, template_name="registerPage.html", context={"register_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    return redirect("/")
