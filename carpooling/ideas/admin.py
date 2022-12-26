90b3c88d466caf067949789dd4fd66ece6ae5569
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2022-12-26 13:46:39 +0100

    add car related models

--- carpooling/ideas/admin.py
+++ carpooling/ideas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, City, Ride
+from .models import User, City, Ride, CarBrand, CarModel
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(City)
 admin.site.register(Ride)
+admin.site.register(CarBrand)
+admin.site.register(CarModel)
